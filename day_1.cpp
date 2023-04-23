dcab2f959e8dbb7868cd8a9db8248646f11b177a
Pradeep Bansiwal <46450532+pradeeps9@users.noreply.github.com>
2023-04-23 17:16:35 +0530

    Arrays_day_1
    Introduction to array by printing array, and finding Min-Max using linear search. 
    Getting familiarized with arrays by printing Extreme point and Reversing array by different swap methods.

--- day_1.cpp
+++ day_1.cpp
@@ -32,13 +32,70 @@ int min (int arr[], int size) {
   return minM;
 }
 
+void printExtreme (int arr[], int size){
+
+  int start = 0, end = size-1;
+  while(start<=end) {
+    if(start!=end){
+      cout << arr[start] << " " << arr[end] << " ";
+    }
+    else{
+      cout << arr[start] << " ";
+    }
+    start++;
+    end--;
+  }
+  
+}
+
+void reverseArr(int arr[], int size) {
+
+  int start = 0, end = size-1;
+  while(start<=end) {
+    if(start!=end){
+      //using xor
+      arr[start] = arr[start] ^ arr[end];
+      arr[end] = arr[start] ^ arr[end];
+      arr[start] = arr[start] ^ arr[end];
+      
+      //using inbuilt swap func
+      // swap(arr[start], arr[end]);
+      
+      //using temp
+      // int temp = arr[start];
+      // arr[start] = arr[end];
+      // arr[end] = temp;
+    }
+    start++;
+    end--;
+  }
+
+  // Print the Reversed Array
+  printArr(arr, size);
+
+}
+
+
 int main() {
 
-  int arr[] = {2,5,7,9,5,7,6,3,8,1,4};
-  size = 11;
-  int maxnum = max(arr, size);
-  cout << "Max num in this arr is: " << maxnum << endl;
-  int minnum = min(arr, size);
-  cout << "Min num in this arr is: " << minnum;
+  int size;
+
+  int arr[] = {0,1,2,3,4,5,6,7,8,9};
+  size=10;
+  // printExtreme(arr,size);
+  cout << "Original Array: \n";
+  printArr(arr, size);
+  cout << "\nReversed Array: \n"; 
+  reverseArr(arr,size);
+  
+
+  
+
+  // int arr[] = {2,5,7,9,5,7,6,3,8,1,4};
+  // size = 11;
+  // int maxnum = max(arr, size);
+  // cout << "Max num in this arr is: " << maxnum << endl;
+  // int minnum = min(arr, size);
+  // cout << "Min num in this arr is: " << minnum;
 
 }
